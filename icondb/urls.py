1ea84941a8d2872490cbd03773e3cda1e8c32be0
T.R.Shashwath <T.R.Shashwath>
2013-09-01 21:37:44 +0530

    Added other masters

--- icondb/urls.py
+++ icondb/urls.py
@@ -2,13 +2,22 @@ from django.views.generic import ListView, DetailView
 from django.conf.urls import patterns, url
 import models
 
+def listView(base, model):
+    return url(base+r'$', ListView.as_view(queryset=model.objects.all()))
+
+def detailView(base, model):
+    return url(base + r'/(?P<pk>\d+)$', DetailView.as_view(model=model))
+
 urlpatterns = patterns('',
-        url(r'icons$', ListView.as_view(
-                queryset=models.Icon.objects.all())),
-        url(r'icon/(?P<pk>\d+)$',
-            DetailView.as_view(
-                model=models.Icon)),
-        url(r'image/(?P<pk>\d+)$',
-            DetailView.as_view(
-                model=models.IconImage)),
-        )
+        listView(r'icons', models.Icon),
+        detailView(r'icon', models.Icon),
+        detailView(r'image', models.IconImage),
+        listView(r'elements', models.Element),
+        detailView(r'element', models.Element),
+        listView(r'icontypes', models.IconType),
+        detailView(r'icontype', models.IconType),
+        listView(r'iconstyles', models.IconStyle),
+        detailView(r'iconstyle', models.IconStyle),
+        listView(r'location', models.LocationMaster),
+        detailView(r'location', models.LocationMaster),
+)
